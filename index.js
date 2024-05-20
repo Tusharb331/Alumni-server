04e6779b347a64155a2929923cc2a070b664531f
ranajunaidhashim <ranajunaidhashim6@gmail.com>
2024-05-20 18:14:22 +0500

    changes

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    // origin: ['http://localhost:5173'],
+    origin: ['https://alumni-client.vercel.app/'],
     methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
     credentials: true,
 }));
