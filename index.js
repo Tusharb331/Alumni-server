fcd8dd7c0fb3fa645643e44b13c2145b14db1b69
ranajunaidhashim <ranajunaidhashim6@gmail.com>
2024-05-21 01:02:06 +0500

    changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { adminRouter } from "./Routes/AdminRoutes.js";
 import dotenv from "dotenv";
+import db from "./utils/db.js";
 
 
 dotenv.config();
