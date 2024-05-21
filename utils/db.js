226b5acf192076db8685fd43740cad79de76907e
ranajunaidhashim <ranajunaidhashim6@gmail.com>
2024-05-21 11:37:51 +0500

    changes

--- utils/db.js
+++ utils/db.js
@@ -3,21 +3,36 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.NAME,
-    port: process.env.DB_PORT 
-})
-// port: process.env.PORT remove port to run offline 
+let connection;
 
-con.connect((err) => {
-    if (err) {
-        console.log("Connection Error", err)
-    } else {
-        console.log("connected")
-    }
-})
+function handleDisconnect() {
+    connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.NAME,
+        port: process.env.DB_PORT
+    });
 
-export default con;
+    connection.connect((err) => {
+        if (err) {
+            console.error("Error connecting to database:", err);
+            setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
+        } else {
+            console.log("Connected to database");
+        }
+    });
+
+    connection.on('error', (err) => {
+        console.error("Database error:", err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET' || err.fatal) {
+            handleDisconnect(); // Reconnect on connection loss
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
+
+export default connection;
