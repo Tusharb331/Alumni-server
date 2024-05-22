dc860a09e1356581f38337eb758333efbe2d27ce
ranajunaidhashim <ranajunaidhashim6@gmail.com>
2024-05-22 13:28:21 +0500

    change

--- utils/db.js
+++ utils/db.js
@@ -24,7 +24,9 @@
 // export default con;
 
 import mysql from "mysql2/promise"; // Import mysql2 package
+import dotenv from "dotenv";
 
+dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
